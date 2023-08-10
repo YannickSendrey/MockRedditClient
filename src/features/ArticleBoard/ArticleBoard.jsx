334e2df67d509682a0673c8c59d6ce6e33c28624
YannickSendrey <yannicksendrey@gmail.com>
2023-08-10 11:35:58 +0200

    fix: render random articles on homepage only on first render + checking if img src OK

--- src/features/ArticleBoard/ArticleBoard.jsx
+++ src/features/ArticleBoard/ArticleBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Article } from '../../components/Article';
 import { useSelector } from 'react-redux';
 import { selectAllArticles } from './articleBoardSlice';
@@ -7,33 +7,31 @@ export const ArticleBoard = () => {
     
     const articles = useSelector(selectAllArticles);
 
-    // custom way to get 3 random articles using Fisher-Yates sorting algorithm
-    const idsArray = articles.map((article) => {
-        return article.id
-    });
+    // custom way to get 3 random articles using Fisher-Yates sorting algorithm, only on the first render
+     const [randomArticles, setRandomArticles] = useState([]);
 
-    const shuffle = (array) => { 
-        for (let i = array.length - 1; i > 0; i--) { 
-          const j = Math.floor(Math.random() * (i + 1)); 
-          [array[i], array[j]] = [array[j], array[i]]; 
-        } 
-        return array; 
-      }; 
+     const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 
-    shuffle(idsArray);
-    const ids = idsArray.slice(0, 3);
-
-    const randomArticles = articles.filter((article) => {
-        return ids.find(id => id === article.id);
-    })
-    //
+     useEffect(() => {
+         const idsArray = articles.map((article) => article.id);
+         const shuffledIds = shuffle(idsArray);
+         const selectedIds = shuffledIds.slice(0, 3);
+         const selectedArticles = articles.filter((article) => selectedIds.includes(article.id));
+         setRandomArticles(selectedArticles);
+     }, []);
+ 
 
 
     
     return (
         <>
-            {randomArticles.slice(0, 3).map((article) => {
-                // render 3 random articles on homePage
+            {randomArticles.map((article) => {
                 return <Article
                          articleId={article.id}
                          key={article.id}
